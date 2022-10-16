98a0b9c1e147be03f8bcb6acc685f556933a6e97
keemo01 <78498562+keemo01@users.noreply.github.com>
2022-10-16 20:08:32 +0100

    Update Read.js

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,31 +1,56 @@
-import React, {Component} from 'react';
-import { Movies } from './Movies';
-import axios from 'axios';
+import React from "react";
 
-export class Read extends React.Component{
-    //Retrives the json URL data and saves it the mymovies array 
-    componentDidMount(){
-        axios.get('https://jsonblob.com/api/jsonblob/894944504570986496')
-            .then((response)=>{
-                this.setState({
-                    mymovies: response.data.movies
-                })
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-    }
-    //movies[] stores JSON information in array
+//import books
+import {Books} from "./books";
+
+export class Read extends React.Component {
+
+    //will hold all data for class
     state = {
-        mymovies:[] 
-    };
+        //array
+        books: [
+            {
+                "title": "Learn Git in a Month of Lunches",
+                "isbn": "1617292419",
+                "pageCount": 0,
+                "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
+                "status": "MEAP",
+                "authors": ["Rick Umali"],
+                "categories": []
+            },
+            {
+                "title": "MongoDB in Action, Second Edition",
+                "isbn": "1617291609",
+                "pageCount": 0,
+                "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg", "status": "MEAP",
+                "authors": [
+                    "Kyle Banker",
+                    "Peter Bakkum",
+                    "Tim Hawkins",
+                    "Shaun Verch",
+                    "Douglas Garrett"
+                ],
+                "categories": []
+            },
+            {
+                "title": "Getting MEAN with Mongo, Express, Angular, and Node",
+                "isbn": "1617292036",
+                "pageCount": 0,
+                "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg", "status": "MEAP",
+                "authors": ["Simon Holmes"],
+                "categories": []
+            }
+        ]
 
-    //Allows us to store the array information
-    render(){
-        return(
+
+    }
+
+    //must rertun jss code
+    render() {
+        return (
             <div>
-                <h3>Hello from read component</h3>
-                <Movies films={this.state.mymovies}></Movies>
+                <h3>Hello from my Read component!</h3>
+                <Books books={this.state.books}></Books>
             </div>
         );
     }
